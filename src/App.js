74b1d2a3022aacd15e292c3e1de5fff205678a86
Bhavyalohami <bhavyalohami@gmail.com>
2026-02-07 23:35:09 +0530

    flavors 3

--- src/App.js
+++ src/App.js
@@ -1024,7 +1024,7 @@ const ScrollToTopOnRouteChange = () => {
 
 function App() {
   return (
-    <Router basename="/devaoted_ice_cream">
+    <Router basename="/devoated_ice_cream">
       <Header />
       <div className="min-h-screen overflow-x-hidden">
         {/* This component handles scrolling to top on route change */}
